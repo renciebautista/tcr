be651c01405e22ef81b75e6eff37c475babe5675
Rencie Bautista <rencie.bautista@yahoo.com>
2016-05-10 17:55:53 +0800

    update audit

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\UserSummary;
 use App\Audit;
 use App\User;
+use App\PostedAudit;
 
 class ReportController extends Controller
 {
@@ -16,22 +17,42 @@ class ReportController extends Controller
 
     	$audit = Audit::findOrFail($audit_id);
         $user = User::findOrFail($user_id);
-        $usersummary = UserSummary::getSummary($audit,$user);
 
-    	$detail = $usersummary->detail;
-    	// $stores = $usersummary->stores;
+        $posted_audit = PostedAudit::where('user_id',$user_id)
+            ->where('audit_id',$audit->id)->get();
 
-    	return json_encode((array)$detail);
+        if($posted_audit->count() > 1){
+            $usersummary = UserSummary::getSummary($audit,$user);
+
+        	$detail = $usersummary->detail;
+
+        	return json_encode((array)$detail);
+
+        }else{
+            $data['msg'] = "No Report Available";
+            return json_encode($data);
+        }
     }
 
     public function getStoreSummary($audit_id,$user_id){
-
+        
     	$audit = Audit::findOrFail($audit_id);
         $user = User::findOrFail($user_id);
-        $usersummary = UserSummary::getSummary($audit,$user);
 
-    	$stores = $usersummary->stores;
+        $posted_audit = PostedAudit::where('user_id',$user_id)
+            ->where('audit_id',$audit->id)->get();
 
-    	return $stores->toJson();
+        if($posted_audit->count() > 1){
+            $usersummary = UserSummary::getSummary($audit,$user);
+
+            $stores = $usersummary->stores;
+            $stores->msg_status = 1;
+
+            return $stores->toJson();
+        }else{
+            $data['msg'] = "No Report Available";
+            return json_encode($data);
+        }
+       
     }
 }
